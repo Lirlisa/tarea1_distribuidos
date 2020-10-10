59b80cc290aefb207cdd1c4589d3eed6a7b2c788
Lirlisa <mati_67m@hotmail.com>
2020-10-10 17:21:47 -0300

    creada comuniación base entre cliente y logistica

--- logistica/pedido/pedido.go
+++ logistica/pedido/pedido.go
@@ -10,8 +10,14 @@ type Server struct {
 }
 
 func (s *Server) Encargar(ctx context.Context, in *Encargo) (*Encargo, error) {
-	log.Printf("Se ha recibido encargo")
-	return in, nil
+	log.Printf("Se ha recibido encargo: %s", in.TipoLocal)
+	return &Encargo{
+		TipoLocal:      "xd",
+		NombreProducto: "xd",
+		Valor:          0,
+		Origen:         "xd",
+		Destino:        "xd",
+	}, nil
 }
 func (s *Server) EstadoEncargo(ctx context.Context, in *Producto) (*Producto, error) {
 	log.Printf("Solicitado estado de %s", in.ID)
