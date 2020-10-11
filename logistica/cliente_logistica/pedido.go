f68d7f4bae11484553afc8da8e09b047cdb194ac
Lirlisa <mati_67m@hotmail.com>
2020-10-11 16:17:26 -0300

    modificado equema de pedido.proto

--- logistica/cliente_logistica/pedido.go
+++ logistica/cliente_logistica/pedido.go
@@ -1,4 +1,4 @@
-package pedido
+package cliente_pedido
 
 import (
 	"log"
@@ -6,18 +6,14 @@ import (
 	"golang.org/x/net/context"
 )
 
+
+
 type Server struct {
 }
 
 func (s *Server) Encargar(ctx context.Context, in *Encargo) (*Encargo, error) {
 	log.Printf("Se ha recibido encargo: %s", in.TipoLocal)
-	return &Encargo{
-		TipoLocal:      "xd",
-		NombreProducto: "xd",
-		Valor:          0,
-		Origen:         "xd",
-		Destino:        "xd",
-	}, nil
+	
 }
 func (s *Server) EstadoEncargo(ctx context.Context, in *Producto) (*Producto, error) {
 	log.Printf("Solicitado estado de %s", in.ID)
