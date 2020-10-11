089ce16d7bb042bac3c6932b07ca563cffc73b04
Lirlisa <mati_67m@hotmail.com>
2020-10-11 17:41:42 -0300

    extendida funcionalidad de pedido.go para adaptarse al nuevo proto

--- logistica/logistica.go
+++ logistica/logistica.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"time"
 
-	"./pedido"
+	"./ClienteLogistica"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	//setear seed para los números de seguimiento
+	rand.Seed(time.Now().UnixNano())
+
 	listener, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatalf("Se ha producido un error: %s", err)
@@ -17,9 +22,9 @@ func main() {
 
 	fmt.Println("Iniciado servidor en escucha en puerto 9000")
 
-	servidor := pedido.Server{}
+	servidor := ClienteLogistica.Server{}
 	grpcServer := grpc.NewServer()
-	pedido.RegisterInteraccionesServer(grpcServer, &servidor)
+	ClienteLogistica.RegisterInteraccionesServer(grpcServer, &servidor)
 
 	if err := grpcServer.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %s", err)
