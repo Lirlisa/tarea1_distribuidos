77059792da4ed4e84ecf0cffcc8e4f4f143459a5
Lirlisa <mati_67m@hotmail.com>
2020-10-16 00:39:41 -0300

    intento de conectar con rabbitmq

--- logistica/logistica.go
+++ logistica/logistica.go
@@ -11,6 +11,7 @@ import (
 
 	"./CamionLogistica"
 	"./ClienteLogistica"
+	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 )
 
@@ -30,7 +31,7 @@ func main() {
 
 	fmt.Println("Iniciado servidor en escucha en puerto 9000")
 
-	wait.Add(2)
+	wait.Add(3)
 	go func() {
 		escuchar_cliente(listenerCliente)
 		wait.Done()
@@ -39,6 +40,11 @@ func main() {
 		escuchar_camion(listenerCamion)
 		wait.Done()
 	}()
+	go func() {
+		conectar_finanas()
+		wait.Done()
+	}()
+
 	wait.Wait()
 }
 
@@ -60,4 +66,42 @@ func escuchar_camion(listener net.Listener) {
 	if err := grpcServer.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %s", err)
 	}
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func conectar_finanas() {
+	conn, err := amqp.Dial("amqp://test:test@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
 }
